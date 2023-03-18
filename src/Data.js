ba6f3f423620da4a413b307eb5b6967d0fe3e888
Rana Nassar <rana.nassar99@yahoo.com>
2023-03-18 18:40:47 +0200

    third commit

--- src/Data.js
+++ src/Data.js
@@ -2,25 +2,25 @@ const Data = [
    {
    cardTitle: "Mira Italian Cuisine",
    description: " this is one of my favourite projects website, this website is for an italian cuisine Restaurant where you can check the menu, order online and make reservation too.",
-   img: "Rana-portfolio/images/project1.PNG",
+   img: "./Rana-portfolio/images/project1.PNG",
    URL: "https://rananassar2021.github.io/mira-italian-cuisine/index.html"
 },
 {
    cardTitle: "Able Foundation",
    description: "This website is called able website and it is a website designed to support people with physical disabilities by offering them different kind of services.",
-   img: "Rana-portfolio/images/project2.PNG",
+   img: "./Rana-portfolio/images/project2.PNG",
    URL: "https://rananassar2021.github.io/Able/"
 },
 {
    cardTitle: "React website template",
    description: "This website is a template cool webiste i made it usinig React that can be reused for any other purpose too",
-   img: "Rana-portfolio/images/project3.PNG",
+   img: "./Rana-portfolio/images/project3.PNG",
    URL: "https://rananassar2021.github.io/react-template/"
 },
 {
    cardTitle: "Mangata & Gallo",
    description: "This website is for jewelry with the latest updates of Mangato & Gallo website where the latest designs of the brand is updated . ",
-   img: "Rana-portfolio/images/project4.PNG",
+   img: "./Rana-portfolio/images/project4.PNG",
    URL: "https://rananassar2021.github.io/mangata-gallo/"
 }
 
